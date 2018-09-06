c6edbc835f75fe995f3a75fc14c5e4dc0fca666c
Rory <wehdy224@gmail.com>
2018-09-06 17:09:23 +0800

    update webpack config settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,40 @@
 "use strict";
-const devMode = process.env.NODE_ENV !== 'production';
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const webpack = require('webpack');
 
-module.exports = {
-    entry: path.resolve(__dirname, './src/index.js'),
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        hot: true,
-        compress: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.pug$/,
-                use: ['pug-loader']
-            },
-            {
-                test: /\.(sa|sc|c)ss$/,
-                use: [ devMode ? 'style-loader' : MiniCssExtractPlugin.loader,'style-loader', 'css-loader', 'sass-loader']
-            }
-        ]
-    },
-    plugins: [
-        new CleanWebpackPlugin(['dist']),
-        new MiniCssExtractPlugin({
-            filename: "[name].[hash].css",
-        }),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, './src/index.pug'),
-            filename: 'index.html'
-        }),
-        new webpack.HotModuleReplacementPlugin()
-    ],
-    output: {
-        filename: 'app.[hash].js',
-        path: path.resolve(__dirname, 'dist')
-    },
-
+module.exports = (env, argv) =>{
+    const devMode = argv.mode !== 'production';
+    const path = require('path');
+    const HtmlWebpackPlugin = require('html-webpack-plugin');
+    const CleanWebpackPlugin = require('clean-webpack-plugin');
+    const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+    const webpack = require('webpack');
+    return {
+        module: {
+            rules: [
+                {
+                    test: /\.pug$/,
+                    use: ['pug-loader']
+                },
+                {
+                    test: /\.(sa|sc|c)ss$/,
+                    use: [ devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                            'css-loader',
+                            'sass-loader']
+                }
+            ]
+        },
+        plugins: [
+            new CleanWebpackPlugin(['dist']),
+            new MiniCssExtractPlugin({
+                filename: "app.[hash].css",
+            }),
+            new HtmlWebpackPlugin({
+                template: path.resolve(__dirname, './src/index.pug'),
+                filename: 'index.html'
+            }),
+            new webpack.HotModuleReplacementPlugin()
+        ],
+        output: {
+            filename: 'app.[hash].js'
+        }
+    }
 };
